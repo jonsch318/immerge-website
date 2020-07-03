911a0d8cd2f8f81f816e8e4d057cd091f9346ac3
Jonas Schneider <jonas.max.schneider@gmail.com>
2020-07-03 15:27:43 +0200

    Added header-menu component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from "./containers/app/app.component";
 import { NavbarComponent } from "./containers/navbar/navbar.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { LogoComponent } from "./components/logo/logo.component";
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 
 @NgModule({
     imports: [CommonModule, RouterModule, MatToolbarModule],
-    declarations: [AppComponent, NavbarComponent, LogoComponent],
+    declarations: [AppComponent, NavbarComponent, LogoComponent, HeaderMenuComponent],
     exports: [],
 })
 export class CoreModule {
