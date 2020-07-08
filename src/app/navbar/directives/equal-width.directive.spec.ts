97b193f82222048b2953acbfdb0dfd29cf8ede75
Jonas Schneider <jonas.max.schneider@gmail.com>
2020-07-08 12:46:58 +0200

    Decluttering Navbar for final release

--- src/app/navbar/directives/equal-width.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EqualWidthDirective } from './equal-width.directive';
-
-describe('EqualWidthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EqualWidthDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,38 +1,12 @@
 import { NgModule } from "@angular/core";
-import { NavbarComponent } from "./containers/navbar/navbar.component";
-import { LogoComponent } from "./components/logo/logo.component";
-import { HeaderMenuComponent } from "./containers/header-menu/header-menu.component";
-import { RouterModule } from "@angular/router";
-import { IdentityComponent } from "./components/identity/identity.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { EqualWidthDirective } from "./directives/equal-width.directive";
 import { CommonModule } from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { OverlayModule } from "@angular/cdk/overlay";
-import { DropdownComponent } from "./components/dropdown/dropdown.component";
-import { MatMenuModule } from "@angular/material/menu";
-import { ProjectDropdownComponent } from './containers/project-dropdown/project-dropdown.component';
+import { RouterModule } from "@angular/router";
+import { CoreModule } from "../core/core.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
-    RouterModule,
-    MatButtonModule,
-    MatIconModule,
-    OverlayModule,
-    MatMenuModule,
-  ],
-  declarations: [
-    NavbarComponent,
-    LogoComponent,
-    HeaderMenuComponent,
-    IdentityComponent,
-    EqualWidthDirective,
-    DropdownComponent,
-    ProjectDropdownComponent,
-  ],
-  exports: [NavbarComponent],
+  imports: [CommonModule, BrowserAnimationsModule, RouterModule, CoreModule],
+  declarations: [],
+  exports: [],
 })
 export class NavbarModule {}
