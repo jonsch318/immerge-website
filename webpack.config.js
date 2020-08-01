7f3733ac17a9c6ed35aeddc1d4bec0e049f511d3
Jonas Schneider <jonas.max.schneider@gmail.com>
2020-08-01 21:19:13 +0200

    added postcss and created material theming

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    }
+}
+Options
