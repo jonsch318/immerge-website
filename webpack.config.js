648cf2e982983ac22989d496d66ea1cc39846341
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-08-04 20:42:05 +0200

    removed css and style loader to get postcss working

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
+const WebpackNotifierPlugin = require("webpack-notifier");
+const path = require("path");
+
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { importLoaders: 1 } },
-                    'postcss-loader'
+                   'postcss-loader'
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new WebpackNotifierPlugin({
+            alwaysNotify: true,
+            title: "Immerge-Homepage",
+            contentImage: path.join(__dirname, "src/assets/favicon/favicon-32x32.png")
+        }),
+    ]
 }
